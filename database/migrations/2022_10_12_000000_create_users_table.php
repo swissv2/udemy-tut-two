4bd39667eca4da9f6e8c35551e8706541fea024a
swissv2 <swissv2@users.noreply.github.com>
2022-08-05 20:56:49 -0700

    Create Roles and Permission Database Implementation
    Using  php artisan make:model and make:migration for roles and permissions. Instructions within text document.

---
+++ database/migrations/2022_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            //Add foreign key for the Roles of a user (Admin, Member, Etc)
+            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
